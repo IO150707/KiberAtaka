c01c89e382c0b31e5c39d4b2dc0a22131498fe61
IO150707 <70840827+IO150707@users.noreply.github.com>
2021-01-17 23:13:02 +0300

    Done

---
+++ script.js
@@ -0,0 +1,10 @@
+const net = document.querySelector("#dont");
+const not = document.querySelector("#dont1");
+
+net.addEventListener("click", function() {
+  alert("Извините, но эта вкладка пока что не доступна...");
+});
+
+not.addEventListener("click", function() {
+  alert("Извините, но эта вкладка пока что не доступна...");
+});
